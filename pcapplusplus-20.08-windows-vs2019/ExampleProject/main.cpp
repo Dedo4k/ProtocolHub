9f5a6bc22b517e7cdf8b7b6c2d5b1a2af02619d7
Asinovich Sergeu <asinovich2015@mail.ru>
2021-04-21 19:25:38 +0300

    First commit. Added .gitignore. And added parser protocols(UPD, TCP, IPv4, ...)

--- pcapplusplus-20.08-windows-vs2019/ExampleProject/main.cpp
+++ pcapplusplus-20.08-windows-vs2019/ExampleProject/main.cpp
@@ -12,10 +12,33 @@
 #include <stdlib.h>
 #include <SystemUtils.h>
 
+std::string printTcpFlags(pcpp::TcpLayer* tcpLayer)
+{
+	std::string result = "";
+	if (tcpLayer->getTcpHeader()->synFlag == 1)
+		result += "SYN ";
+	if (tcpLayer->getTcpHeader()->ackFlag == 1)
+		result += "ACK ";
+	if (tcpLayer->getTcpHeader()->pshFlag == 1)
+		result += "PSH ";
+	if (tcpLayer->getTcpHeader()->cwrFlag == 1)
+		result += "CWR ";
+	if (tcpLayer->getTcpHeader()->urgFlag == 1)
+		result += "URG ";
+	if (tcpLayer->getTcpHeader()->eceFlag == 1)
+		result += "ECE ";
+	if (tcpLayer->getTcpHeader()->rstFlag == 1)
+		result += "RST ";
+	if (tcpLayer->getTcpHeader()->finFlag == 1)
+		result += "FIN ";
+
+	return result;
+}
+
 int main(int argc, char* argv[])
 {
 	// open a pcap file for reading
-	pcpp::PcapFileReaderDevice reader("ex.pcap");
+	pcpp::PcapFileReaderDevice reader("test2.pcap");
 	if (!reader.open())
 	{
 		printf("Error opening the pcap file\n");
@@ -23,12 +46,14 @@ int main(int argc, char* argv[])
 	}
 
 	// read the packets from the file
+	int counter = 0;
 	pcpp::RawPacket rawPacket;
 	while (reader.getNextPacket(rawPacket))
 	{
 		// parse the raw packet into a parsed packet
 		pcpp::Packet parsedPacket(&rawPacket);
 
+		//parse UDP protocol 
 		if (parsedPacket.isPacketOfType(pcpp::UDP))
 		{
 			pcpp::UdpLayer* udp = parsedPacket.getLayerOfType<pcpp::UdpLayer>();
@@ -38,6 +63,7 @@ int main(int argc, char* argv[])
 			printf("Destination UDP port: %d\n", (int)udp->getUdpHeader()->portDst);
 			printf("Length of header and playload in bytes: %d\n", (int)udp->getUdpHeader()->length);
 		}
+		//parse UDP protocol 
 		else if (parsedPacket.isPacketOfType(pcpp::TCP))
 		{
 			pcpp::TcpLayer* tcp = parsedPacket.getLayerOfType<pcpp::TcpLayer>();
@@ -46,7 +72,9 @@ int main(int argc, char* argv[])
 			printf("Source TCP port: %d\n", (int)tcp->getTcpHeader()->portSrc);
 			printf("Destination TCP port: %d\n", (int)tcp->getTcpHeader()->portDst);
 			printf("Window size: %d\n", (int)tcp->getTcpHeader()->windowSize);
+			printf("TCP flags: %s\n", printTcpFlags(tcp).c_str());
 		}
+		//parse UDP protocol 
 		else if (parsedPacket.isPacketOfType(pcpp::DNS))
 		{
 			pcpp::DnsLayer* dns = parsedPacket.getLayerOfType<pcpp::DnsLayer>();
@@ -55,6 +83,7 @@ int main(int argc, char* argv[])
 			printf("DNS authentic data: %d\n", (int)dns->getDnsHeader()->authenticData);
 			printf("DNS transaction ID: %d\n", (int)dns->getDnsHeader()->transactionID);
 		}
+		//parse UDP protocol 
 		else if (parsedPacket.isPacketOfType(pcpp::HTTP))
 		{
 			pcpp::HttpRequestLayer* httpRq = parsedPacket.getLayerOfType<pcpp::HttpRequestLayer>();
@@ -67,6 +96,7 @@ int main(int argc, char* argv[])
 			printf("HTTP cookie: %s\n", httpRq->getFieldByName(PCPP_HTTP_COOKIE_FIELD)->getFieldValue().c_str());
 			printf("HTTP full URL: %s\n", httpRq->getUrl().c_str());
 		}
+		//parse UDP protocol 
 		else if (parsedPacket.isPacketOfType(pcpp::Ethernet))
 		{
 			pcpp::EthLayer* eth = parsedPacket.getLayerOfType<pcpp::EthLayer>();
@@ -76,6 +106,7 @@ int main(int argc, char* argv[])
 			printf("Ethrenet source MAC address: %d\n", (int)eth->getEthHeader()->srcMac);
 			printf("Ethrenet destination MAC address: %d\n", (int)eth->getEthHeader()->dstMac);
 		}
+		//parse UDP protocol 
 		else if (parsedPacket.isPacketOfType(pcpp::IPv4))
 		{
 			pcpp::IPv4Layer* ip4 = parsedPacket.getLayerOfType<pcpp::IPv4Layer>();
@@ -86,9 +117,12 @@ int main(int argc, char* argv[])
 			printf("IP ID: 0x%X\n", ip4->getIPv4Header()->ipId);
 			printf("TTL (eight-bit): %d\n", (int)ip4->getIPv4Header()->timeToLive);
 		}
+		//parse UDP protocol 
 		else
 			printf("\nIDK! But not for a long time\n");
-
+		
+		counter++;
+		printf("\n%d", counter);
 	}
 	// close the file
 	reader.close();
